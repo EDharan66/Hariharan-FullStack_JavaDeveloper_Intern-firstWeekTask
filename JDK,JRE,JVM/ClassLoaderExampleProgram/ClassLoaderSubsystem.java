066a552d3c29cfb1e6570ceff9c1031f82b2ea9d
Hariharan <hariharan80122@gmail.com>
2021-08-04 15:05:23 +0530

    day 1 week task

---
+++ JDK,JRE,JVM/ClassLoaderExampleProgram/ClassLoaderSubsystem.java
@@ -0,0 +1,11 @@
+public class ClassLoaderSubsystem {
+    public static void main(String[] args)
+    {
+        // String class is loaded by bootstrap loader, and
+        // bootstrap loader is not Java object, hence null
+        System.out.println(String.class.getClassLoader());
+
+        // ClassLoaderSubsystem class is loaded by Application loader
+        System.out.println(ClassLoaderSubsystem.class.getClassLoader());
+    }
+}
